45815d4dc2a31dcc6064aba6b382fcf9307ef63a
Frank T Kagiyama <frank.kagiyama@gmail.com>
2015-05-18 11:23:27 -0700

    changes to everything

---
+++ function.js
@@ -0,0 +1,9 @@
+/// <reference path="typings/jquery/jquery.d.ts"/> // allows it to become jquery
+
+$(".people . toggle").click(function() {
+	var $this = $(this); /* adding $ what is this? */
+	
+	$this.parents("li").toggleClass("expanded");
+	$this.parents(" .people").toggleClass("solo");
+
+});
