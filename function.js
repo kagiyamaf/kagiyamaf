48bee9c1f3d22f912625dc4704682a0efa3efb53
Frank T Kagiyama <frank.kagiyama@gmail.com>
2015-05-29 12:35:58 -0700

    everything works, compiled scss

--- function.js
+++ function.js
@@ -3,7 +3,7 @@
 $(".people .toggle").click(function() {
 	var $this = $(this); /* adding $ what is this? */
 	
-	$this.parents("li").toggleClass("expanded");
-	$this.parents(".people").toggleClass("solo");
+	$this.parents("li").toggleClass("expanded"); // li gets expanded and people gets solo
+	$this.parents(".people").toggleClass("solo"); // when it's one person
 
 });
