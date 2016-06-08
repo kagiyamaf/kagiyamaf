82795a38289857dc6ff7350b6423f3b0e4142d7d
Frank T Kagiyama <frank.kagiyama@gmail.com>
2016-06-07 18:45:17 -0700

    new website launch soon

---
+++ slideshow.js
@@ -0,0 +1,21 @@
+var index = 0;
+slide_anim();
+
+function slide_anim()
+{
+    var i;
+    var x = document.getElementsByClassName("slides");
+    for(i = 0; i < x.length; i++)
+    {
+        x[i].style.display = "none";
+    }
+    index++;
+    
+    if(index > x.length)
+    {
+        index = 1
+    }
+    
+    x[index-1].style.display = "block";
+    setTimeout(slide_anim, 2000);
+}
